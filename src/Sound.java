9868392f9043c0f083653d208820b57ca647e40a
Andreeva12 <andreevagercog@gmail.com>
2024-02-29 19:32:02 +0200

    first commit

---
+++ src/Sound.java
@@ -0,0 +1,11 @@
+public class Sound {
+    public static void main(String args[]) {
+        double echo;
+        echo = 7.2 / 2;
+        double dist;
+        dist = 1100 * echo;
+        System.out.println("Расстояние до  скалы " +
+                "составляет " + dist + " метров.");
+    }
+}
+
