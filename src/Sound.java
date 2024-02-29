30f1d5d4e412925443fa51d42767f5250823c182
Andreeva12 <andreevagercog@gmail.com>
2024-02-29 21:35:50 +0200

    added .idea to .gitignore

--- src/Sound.java
+++ src/Sound.java
@@ -1,11 +1,14 @@
+import java.util.Scanner;
+
 public class Sound {
     public static void main(String args[]) {
-        double echo;
-        echo = 7.2 / 2;
-        double dist;
-        dist = 1100 * echo;
+        double speed = 1100;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите время до прихода эха (в сек): ");
+        double echo = scanner.nextDouble();
+        double dist = (speed * echo) /2;
         System.out.println("Расстояние до  скалы " +
-                "составляет " + dist + " метров.");
+                "составляет " + dist + " футов.");
     }
 }
 
