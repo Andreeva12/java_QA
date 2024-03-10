2be657e43c55b01a8e780573d3f362618d278f9f
Andreeva12 <andreevagercog@gmail.com>
2024-03-10 19:54:47 +0200

    added .idea to .gitignore

---
+++ src/GalToLit/GalToLitTable.java
@@ -0,0 +1,24 @@
+package GalToLit;
+
+public class GalToLitTable {
+    public static void rnain (String args []) {
+        double gallons, liters;
+        int counter;
+        counter = 0;
+
+        // Счетчик строк инициализируется нулевым значением
+        for (gallons = 1; gallons <= 100; gallons++) {
+            liters = gallons * 3.7854; // преобразовать в литры
+            System.out.println(gallons + " галлонам соответствует " +
+                    liters + " liters.");
+            counter++;
+
+            //Увеличивать значение счетчика строк на 1 на каждой итерации цикла
+            if (counter == 10) { //Если значение счетчика равно 10, вывести пустую строку
+                System.out.println();
+                counter = 0; // сбросить счетчик строк
+            }
+        }
+    }
+
+}
