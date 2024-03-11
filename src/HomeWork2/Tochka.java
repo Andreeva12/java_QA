1e3033564bdf8915e80f94c8508ff7b32f5a0e89
Andreeva12 <andreevagercog@gmail.com>
2024-03-11 20:25:25 +0200

    added .idea to .gitignore

--- src/HomeWork2/Tochka.java
+++ src/HomeWork2/Tochka.java
@@ -1,5 +1,24 @@
 package HomeWork2;
 
-public class Tochka {
+import java.util.Scanner;
 
-}
+public class Tochka {
+    public static void main(String [] args) {
+        int space = 0;
+        char ch;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите числа (если хотите завершить ввод введите (.): ");
+        while (true){
+            String input = scan.next();
+            if (input.equals(".")) {
+                break;
+            } else {
+                space++;
+            }
+
+        }
+        System.out.println("Пробелов=" + space);
+                }
+    }
+
+
