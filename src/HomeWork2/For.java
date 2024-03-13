59e7253a4e657a048740a3a11dc081a98b9cc76a
Andreeva12 <andreevagercog@gmail.com>
2024-03-13 20:01:37 +0200

    added .idea to .gitignore

---
+++ src/HomeWork2/For.java
@@ -0,0 +1,10 @@
+package HomeWork2;
+//цикл for, в котором перебирались бы значения от 1000 до 0 с шагом -2.
+public class For {
+    public static void main(String[] args) {
+        for (int i=1000; i>=0; i-=2) {
+            System.out.println("Elements: " + i);
+        }
+    }
+
+}
