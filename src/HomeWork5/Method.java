fb48a8fe1841857be3ac919aba1799645a01d51b
Andreeva12 <andreevagercog@gmail.com>
2024-03-18 18:06:27 +0200

    added .idea to .gitignore

---
+++ src/HomeWork5/Method.java
@@ -0,0 +1,16 @@
+package HomeWork5;
+
+import java.util.Scanner;
+
+public class Method {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Введите значение стороны квадрата:");
+        int side = scan.nextInt();
+
+        Square square = new Square(side);
+
+        System.out.println("Площадь квадрата со стороной " + side + ": " + square.calculateArea());
+    }
+}
---
+++ src/HomeWork5/Square.java
@@ -0,0 +1,15 @@
+package HomeWork5;
+
+public class Square {
+    private int side;
+
+    public Square(int side) {
+        this.side = side;
+    }
+
+    public int calculateArea() {
+        return side * side;
+    }
+}
+
+
